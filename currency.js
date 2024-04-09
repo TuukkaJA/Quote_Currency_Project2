c5c4ef6bab87dc40658e92323650c19564af9311
TuukkaJA <e2301995@edu.vamk.fi>
2024-04-09 16:54:32 +0300

    var muutettu let (modernimpi)

--- currency.js
+++ currency.js
@@ -2,16 +2,16 @@ document.getElementById('currencyForm').addEventListener('submit', function(even
     event.preventDefault(); // Prevent form submission
 
     // Get user inputs
-    var fromCurrency = document.getElementById('fromCurrency').value;
-    var toCurrency = document.getElementById('toCurrency').value;
-    var amount = document.getElementById('amount').value;
+    let fromCurrency = document.getElementById('fromCurrency').value;
+    let toCurrency = document.getElementById('toCurrency').value;
+    let amount = document.getElementById('amount').value;
 
     // API call to perform currency conversion
     fetch('https://api.exchangerate-api.com/v4/latest/' + fromCurrency)
         .then(response => response.json())
         .then(data => {
-            var conversionRate = data.rates[toCurrency];
-            var convertedAmount = amount * conversionRate;
+            let conversionRate = data.rates[toCurrency];
+            let convertedAmount = amount * conversionRate;
 
             // Display result
             document.getElementById('result').innerHTML = amount + ' ' + fromCurrency + ' = ' + convertedAmount.toFixed(2) + ' ' + toCurrency;
