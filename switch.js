fcb0d81ada65ddc9b234020e584936479a9b2c19
TuukkaJA <e2301995@edu.vamk.fi>
2024-04-20 18:24:53 +0300

    switch function

--- switch.js
+++ switch.js
@@ -1,3 +1,7 @@
 function switchCurrencies () {
-    
+    let fromCurrency = document.getElementById("fromCurrency").value;
+    let toCurrency = document.getElementById("toCurrency").value;
+
+    document.getElementById("fromCurrency").value = toCurrency;
+    document.getElementById("toCurrency").value = fromCurrency;
 }
